b5eeab34c0a63d32bbbe2ee429940bbb4f578990
RutujaChinchole <rutujachinchole06@gmail.com>
2023-04-20 22:17:50 +0530

    done

---
+++ newrepo.java
@@ -0,0 +1,12 @@
+package pattern;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello java");
+            System.out.println("rutuja");
+            System.out.println("rutuja");
+
+	}
+
+}
